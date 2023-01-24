36abd90424b38215258895d5d2ce4a38a84f7d71
Andry Salazar <avesalazar4@gmail.com>
2023-01-24 15:26:23 -0500

    instalación de firebase

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,11 @@
 // aqui exportaras las funciones que necesites
 
+import {app} from './firebase-app.js';
+
 export const myFunction = () => {
   // aqui tu codigo
   console.log('Hola mundo!');
 };
+
+
+console.log(app);

709aa841de6621ce699537a7eaf40fb6a2ab1fad
Andry Salazar <avesalazar4@gmail.com>
2023-01-27 11:34:20 -0500

    Optimización login

--- src/view/cakebook.js
+++ src/view/cakebook.js
@@ -2,7 +2,7 @@ export default () => {
   const viewHome = `<h2 class= text-center> Posts </h2>`
 
   const divElement = document.createElement('div');
-  divElement.innerHTML = viewHome;
 
+  divElement.innerHTML = viewHome;
   return divElement;
 };

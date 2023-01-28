29b130d02c5b7d0d36545e656f73915488ecbf78
Andry Salazar <avesalazar4@gmail.com>
2023-01-28 14:43:09 -0500

    feature: styles Login

--- src/view/login.js
+++ src/view/login.js
@@ -1,6 +1,6 @@
 export default () => {
   const viewLogin = /* html */ `
-   <div id="container" class="login-container">
+   <div class="login-container">
     <div class="img-container">
       <picture>
         <source media="(max-width: 599px)" srcset="/img/back-login.png" />
@@ -8,13 +8,13 @@ export default () => {
         <img src="/img/back-login-desktop.png" alt="" />
       </picture>
     </div>
-    <div id= "container1">
+    <div class= "container1">
       <h1>cakeBook</h1>
       <div class= "form-btn">
-        <input class= "input-users" type="text" placeholder="Usuario">
-        <input class= "input-users" type="text" id="contraseña"  placeholder="Contraseña">
-        <button type = "submit" class="log-button" id="login"><strong>INICIAR SESIÓN</strong></button>
-        <button  class="log-button" id="register"><strong>REGISTRARSE</strong></button>
+        <input class= "input-users" type="text" name="username" id="username" placeholder="Usuario">
+        <input class= "input-users" type="password" name="password" id="password"  placeholder="Contraseña">
+        <button class="log-button" id="login">INICIAR SESIÓN</button>
+        <button class="log-button" id="register">REGISTRARSE</button>
         <div class="login-google">
           <span>Ingresar con:</span>
           <button id="btn-google-register"> 

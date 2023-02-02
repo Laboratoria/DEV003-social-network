19e8964fc7267ac9dfb922216a17188e5ba7685f
Andry Salazar <avesalazar4@gmail.com>
2023-02-02 14:06:45 -0500

    feat: route redirection and refactor link CSS

--- src/lib/index.js
+++ src/lib/index.js
@@ -4,3 +4,16 @@ export const myFunction = () => {
   console.log('Hola mundo!');
 };
 
+export const addRouteEvents = () => {
+  const oldPushState = history.pushState;
+
+  history.pushState = function pushState(...args) {
+    const ret = oldPushState.apply(this, args);
+    window.dispatchEvent(new Event('locationchange'));
+    return ret;
+  };
+
+  window.addEventListener('popstate', () =>
+    window.dispatchEvent(new Event('locationchange'))
+  );
+};

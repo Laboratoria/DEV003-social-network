106d6a52e357ea7a432622a816296726934759bd
Andry Salazar <avesalazar4@gmail.com>
2023-02-07 13:36:14 -0500

    feat: css- post view-cakebook

--- src/view/cakebook.js
+++ src/view/cakebook.js
@@ -6,28 +6,37 @@ export default () => {
   const viewTimeline = /* html */ `
   <!--<img class = "fondoLogin" src = "images/fondoLogin.png">-->
   <header class="header">
-  <nav class="nav">
-  <h1>cakeBook</h1>
-    <div class="btn-signOut">
-      <button type="button" class="signOut" id="signOut">
-        <i class="fas fa-sign-out-alt"></i>
-      </button>
-    </div>
-  </nav>
+    <nav class="nav">
+      <h1>cakeBook</h1>
+      <div class="btn-signOut">
+        <button type="button" class="signOut" id="signOut">
+          <i class="fas fa-sign-out-alt"></i>
+        </button>
+      </div>
+    </nav>
   </header>
   <div class="containerTimeline">
-  <div class="myPosts">
-    <h2>¿Qué vamos a compartir hoy?</h2>
-    <textarea class="myPosts" id="post" placeholder="" ></textarea>
-    <button class="postBtn">Publicar</button>
-  </div>
-  <div class="friendsPosts">
-    <div class="friendPic">
-    <button type="button" class="userPicBtn"><img src="./img/userPic.png" alt="" class="imgUser"></button>
+    <div class="myPosts">
+       <h2>¿Qué vamos a compartir hoy?</h2>
+        <div class="header-post"> 
+          <div class="img-perfil">
+            <img src="./img/userPic.png" alt="" class="imgUser">
+          </div>
+          <textarea class="myPosts" class="text-post" id="post" placeholder="" ></textarea>
+        </div>
+      <ul class="nav-myPost">
+        <li><button class="postBtn">Foto</button></li>
+        <li><button class="postBtn">Publicar</button></li>
+      </ul> 
     </div>
-    <div class="friendsText">
-    <h2>User</h2>
-    <p>post</p>
+    <div class="friendsPosts">
+      <div class="friendPic">
+        <button type="button" class="userPicBtn"><img src="./img/userPic.png" alt="" class="imgUser"></button>
+      </div>
+      <div class="friendsText">
+        <h2>User</h2>
+        <p>post</p>
+      </div>
     </div>
   </div>
   </div>

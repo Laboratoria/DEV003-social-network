0805e7a27ad267754b21df70fc6086812dcc6660
Adamantine-art <andreita.mosqueira@gmail.com>
2023-02-08 11:18:42 -0300

    Avance de Sign Up con Firebase

--- src/main.js
+++ src/main.js
@@ -12,8 +12,8 @@ const botonGoogle = document.getElementById('google');
 
 botonGoogle.addEventListener('click', () => {
   console.log('registrate');
-  signUp('ejemplo@gmail.com', '123456')
-    .then((userCredential) => {
+  signUp('ejemplo@gmail.com', '123456') //data para probar si está leyendo los datos
+    .then((userCredential) => { //de acá para abajo es código copiado de la pag de firebase
       // Signed in
       const user = userCredential.user;
       console.log(user);

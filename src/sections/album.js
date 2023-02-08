dcf3cb626587f92c0eb1c55ff378afd701829f58
= <=>
2023-02-08 07:52:44 -0500

    feat: cambios en html css y album.js

--- src/sections/album.js
+++ src/sections/album.js
@@ -1,6 +1,20 @@
 export default () => {
   const album = `
-   <h1>ALBUM PAGE</h1>
+  <div id="container">
+  <h1>Lista de álbumes</h1>
+  <div class="container-album">
+    <div id="title-option-album">Álbumes</div>
+      <div id="item-album">
+        <img class="img-album" width="140" height="140"  style="background-color: black;">
+        <label>Nombre </label>
+        <i class="fa-solid fa-heart"></i>
+        <ico> </ico>
+        <ico> </ico>
+
+      </div>
+  </div>
+</div>
+
    `;
 
   const divAlbum = document.createElement('div');
